182b4e7bd35d3867425c9770623d4987d821aaa4
Jorge Meireles <jorgentoniomeireles@gmail.com>
2023-01-08 13:59:28 -0300

    Criação de rotas,controllers e inserção da entidade autor

--- server.js
+++ server.js
@@ -1,25 +1,6 @@
-//inves irei usar o express
-//const http = require("http")
-
 
 import app from './src/app.js';
 const porta = process.env.PORT || 3000;
-// const rotas = {
-//     '/': 'Curso de Node',
-//     '/livros': 'Entrei na pag de livros',
-//     '/autores': 'Listagem de autores',
-//     '/sobre': 'info do projeto'
-
-// }
-// const server = http.createServer((req, res) => {
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.end(rotas[req.url]);
-// })
-
-// server.listen(porta, () =>{
-//     console.log('Servidor está rodando http://localhost:' + porta )
-// })
-
 
 app.listen(porta, () =>{
     console.log('Servidor está rodando http://localhost:' + porta )
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import db from "./config/dbConnect.js";
-import livros from "./models/livro.js";
+import routes from "./routes/index.js"
 
 const app = express();
 db.on("error", console.log.bind(console, 'Error de conexão'))
@@ -8,50 +8,7 @@ db.once("open", ()=> {
     console.log("Conexão com banco feita com sucesso")
 })
 
-app.use(express.json()) // conserte tudo para Json
-
-// const livros = [
-//     {id: 1, "titulo": "Senhor dos Aneis" },
-//     {id: 2, "titulo": "hobbit" }
-// ]
-
-app.get('/', (req, res)=> {
-    res.status(200).send('Curso de node');
-})
-
-app.get('/livros', (req, res)=> {
-    res.status(200).json(livros)
-})
-
-app.post('/livros',(req, res)=>{
-   livros.push(req.body);
-   res.status(201).send('Livro  cadastrado com sucesso')
-
-})
-
-app.put('/livros/:id',(req,res)=>{
-let index = buscaLivro(req.params.id);
-livros[index].titulo = req.body.titulo;
-res.json(livros);
-} )
-
-
-app.get('/livros/:id',(req,res)=>{
-    let index = buscaLivro(req.params.id);
-    res.json(livros[index]);
-    } )
-    
-
-app.delete('/livros/:id',(req,res)=>{
- let index = buscaLivro(req.params.id);
- livros.splice(livros.indexOf(index));
- res.send('elemento removido')
-} )
-        
-
-
-function buscaLivro(id){
-    return livros.findIndex(livro => livro.id == id)
-}
+app.use(express.json()) // converte tudo para Json
+routes(app);
 
 export default app
